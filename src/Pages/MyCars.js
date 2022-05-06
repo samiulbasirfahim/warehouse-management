21a2e1eb4523f8a04e0ab625f83882a610d8fe93
Fahim <samiulbasirfahim360@gmail.com>
2022-05-06 07:22:45 +0600

    my cars responsive done

--- src/Pages/MyCars.js
+++ src/Pages/MyCars.js
@@ -29,7 +29,7 @@ const MyCars = () => {
 		)
 	}
 	return (
-		<div className="bg-white min-h-screen min-w-screen pt-[16vh] md:px-8 px-4 xl:px-26 2xl:px-36">
+		<div className="min-h-screen min-w-screen pt-[16vh] md:px-8 px-4 xl:px-26 2xl:px-36">
 			<ReactHelmet>My cars</ReactHelmet>
 			<p className="text-4xl py-2 text-center font-bold font-mono">
 				My cars

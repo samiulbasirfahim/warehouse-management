9761b5ddc22c8cca814e1151bfcb53c885680e60
Fahim <samiulbasirfahim360@gmail.com>
2022-05-07 13:30:22 +0600

    random car seciton z index problem solved

--- src/Components/RandomCar.js
+++ src/Components/RandomCar.js
@@ -29,12 +29,12 @@ const RandomCar = () => {
 		)
 	}
 	return (
-		<div id="randomCar" className="py-16  border-t-2 border-gray-400">
+		<div id="randomCar" className="py-16 -z-50 border-t-2 border-gray-400">
 			<p className="text-4xl py-8 text-center font-bold font-mono dark:text-white">
 				Random Hot Car
 			</p>
 			<div
-				className="w-full h-[50vh] container mx-auto  bg-center bg-cover flex justify-start bg-no-repeat  lg:rounded-3xl overflow-x-hidden"
+				className="w-full h-[50vh] container mx-auto   bg-center bg-cover flex justify-start bg-no-repeat  lg:rounded-3xl overflow-x-hidden"
 				style={{ backgroundImage: `url(${car?.imgLink})` }}
 			>
 				<div className="bg-slate-100/60 dark:bg-slate-500/70 dark:text-gray-50 lg:pl-8 px xl:pl-36 lg:w-1/2 lg:backdrop-blur-sm grid justify-items-center lg:rounded-l-3xl pb-6">
